6516e1425b09813ef74f3b765306a0370d0e3f3f
Iqboljon Sattoraliyev <sattoraliyeviqboljon0@gmail.com>
2022-09-14 18:51:19 +0500

    1

---
+++ dist/db/collections.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Collection = void 0;
+var Collection;
+(function (Collection) {
+    Collection["USER"] = "users";
+})(Collection = exports.Collection || (exports.Collection = {}));
+// ARRAY_MAX_SIZE
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29sbGVjdGlvbnMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvZGIvY29sbGVjdGlvbnMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBRUEsSUFBWSxVQUVYO0FBRkQsV0FBWSxVQUFVO0lBQ2xCLDRCQUFjLENBQUE7QUFDbEIsQ0FBQyxFQUZXLFVBQVUsR0FBVixrQkFBVSxLQUFWLGtCQUFVLFFBRXJCO0FBQ0QsaUJBQWlCIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgQVJSQVlfTUFYX1NJWkUgfSBmcm9tIFwiY2xhc3MtdmFsaWRhdG9yXCI7XHJcblxyXG5leHBvcnQgZW51bSBDb2xsZWN0aW9uIHtcclxuICAgIFVTRVIgPSAndXNlcnMnXHJcbn1cclxuLy8gQVJSQVlfTUFYX1NJWkUiXX0=
